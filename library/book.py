f3712a4feb8f17af2d828df600ed4fb2f8936125
Sanket <sagrawal5@hawk.iit.edu>
2021-12-10 14:01:34 -0800

    Added models

--- library/book.py
+++ library/book.py
@@ -3,11 +3,12 @@
 # from django.utils import timezone
 
 class Book(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.CharField(max_length=20)
+    edition = models.CharField(max_length=30)
+    # created_date = models.DateTimeField(default=timezone.now)
+    # published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
--- library/models.py
+++ library/models.py
@@ -4,11 +4,124 @@ from django.utils import timezone
 
 
 class Book(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.CharField(max_length=20)
+    edition = models.CharField(max_length=30)
+    # created_date = models.DateTimeField(default=timezone.now)
+    # published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=20)
+    member_type = models.CharField(max_length=30)
+    contactNumber = models.CharField(max_length=20) 
+    # created_date = models.DateTimeField(default=timezone.now)
+    # published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Physical_copies(models.Model):
+    record_id = models.CharField(max_length=100)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class User_bookings(models.Model):
+    member_id = models.CharField(max_length=100)
+    physical_copy_id = models.CharField(max_length=20)
+    booking_date = models.CharField(max_length=30)
+    due_date = models.CharField(max_length=20)
+    status = models.CharField(max_length = 30) 
+    # created_date = models.DateTimeField(default=timezone.now)
+    # published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Records(models.Model):
+    document_type = models.CharField(max_length=100)
+    document_id = models.CharField(max_length=20)
+    quantity = models.CharField(max_length=30)
+    category = models.CharField(max_length=20)
+    # status = models.CharField(max_length = 30) 
+    # created_date = models.DateTimeField(default=timezone.now)
+    # published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Journal_articles(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=20)
+    journal_id = models.CharField(max_length=30)
+    # category = models.CharField(max_length=20)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Magazines(models.Model):
+    title = models.CharField(max_length=100)
+    # author = models.CharField(max_length=20)
+    issue_id = models.CharField(max_length=30)
+    # category = models.CharField(max_length=20)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Journals(models.Model):
+    title = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=20)
+    issue_id = models.CharField(max_length=30)
+    # category = models.CharField(max_length=20)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Magazines(models.Model):
+    title = models.CharField(max_length=100)
+    contributor = models.CharField(max_length=20)
+    published_date = models.CharField(max_length = 30)
+    editor = models.CharField(max_length=30)
+    # category = models.CharField(max_length=20)
 
     def publish(self):
         self.published_date = timezone.now()
