896e14a8c3f5bb907097e4a5c8c16ebbb5b127c4
Sanket <sagrawal5@hawk.iit.edu>
2021-12-10 05:13:11 -0800

    update

--- library/models.py
+++ library/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class Post(models.Model):
+class Book(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField()
--- library/views.py
+++ library/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def book_list(request):
+	return render(request, 'library/book_list.html', {})
