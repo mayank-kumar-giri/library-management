fbca40381470711032e39f2bb0ffac9acdc347fb
Mayank Kumar Giri <mayankkrgiri@gmail.com>
2021-12-11 02:42:34 +0530

    Added initial files

--- library/views.py
+++ library/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Book
 
 # Create your views here.
 def book_list(request):
-	return render(request, 'library/book_list.html', {})
+	books = Book.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'library/book_list.html', {'books' : books})
