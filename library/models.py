f8213a1c995fdae09390655b9e45dab4e6178550
Mayank Kumar Giri <mayankkrgiri@gmail.com>
2021-12-11 04:42:29 +0530

    Updated models

--- library/models.py
+++ library/models.py
@@ -2,63 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=20)
-    edition = models.CharField(max_length=30)
-    record = models.OneToOneField(Record,
-                                    on_delete=models.CASCADE,
-                                    primary_key=True,
-                                )
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
-
-class User(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=20)
-    member_type = models.CharField(max_length=30)
-    contactNumber = models.CharField(max_length=20) 
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
-class PhysicalCopy(models.Model):
-    record_id = models.ForeignKey(record_id, on_delete=models.CASCADE)
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
-class UserBooking(models.Model):
-    user_id = models.ForeignKey(user_id, on_delete=models.CASCADE)
-    physical_copy = models.OneToOneField(PhysicalCopy,
-                                    on_delete=models.CASCADE,
-                                    primary_key=True,
-                                )
-    booking_date = models.CharField(max_length=30)
-    due_date = models.CharField(max_length=20)
-    status = models.CharField(max_length = 30) 
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
 class Record(models.Model):
     document_id = models.CharField(max_length=20)
     document_type = models.CharField(max_length=100)
@@ -72,15 +15,8 @@ class Record(models.Model):
     def __str__(self):
         return self.title
 
-class JournalArticle(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=20)
-    journal_id = models.ForeignKey(models.Journal, on_delete=models.CASCADE)
-    record = models.OneToOneField(Record,
-                                    on_delete=models.CASCADE,
-                                    primary_key=True,
-                                )
-
+class PhysicalCopy(models.Model):
+    record_id = models.ForeignKey(Record, on_delete=models.CASCADE)
     def publish(self):
         self.published_date = timezone.now()
         self.save()
@@ -88,13 +24,15 @@ class JournalArticle(models.Model):
     def __str__(self):
         return self.title
 
-class Magazine(models.Model):
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=20)
+    edition = models.CharField(max_length=30, default='1')
     record = models.OneToOneField(Record,
                                     on_delete=models.CASCADE,
                                     primary_key=True,
+                                    default=1,
                                 )
-    title = models.CharField(max_length=100)
-    issue_id = models.CharField(max_length=30)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -115,14 +53,77 @@ class Journal(models.Model):
     def __str__(self):
         return self.title
 
+class JournalArticle(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=20)
+    journal_id = models.ForeignKey(Journal, on_delete=models.CASCADE)
+    record = models.OneToOneField(Record,
+                                    on_delete=models.CASCADE,
+                                    primary_key=True,
+                                    default=1,
+                                )
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Magazine(models.Model):
+    record = models.OneToOneField(Record,
+                                    on_delete=models.CASCADE,
+                                    primary_key=True,
+                                    default=1,
+                                )
+    title = models.CharField(max_length=100)
+    issue_id = models.CharField(max_length=30)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
 class Issue(models.Model):
     title = models.CharField(max_length=100)
     contributor = models.CharField(max_length=20)
     published_date = models.CharField(max_length = 30)
     editor = models.CharField(max_length=30)
-    journal_id = models.ForeignKey(models.Journal, on_delete=models.CASCADE)
-    magazine_id = models.ForeignKey(models.Magazine, on_delete=models.CASCADE)
+    journal_id = models.ForeignKey(Journal, on_delete=models.CASCADE)
+    magazine_id = models.ForeignKey(Magazine, on_delete=models.CASCADE)
     
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=20)
+    member_type = models.CharField(max_length=30)
+    contactNumber = models.CharField(max_length=20) 
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class UserBooking(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    physical_copy = models.OneToOneField(PhysicalCopy,
+                                    on_delete=models.CASCADE,
+                                    primary_key=True,
+                                )
+    booking_date = models.CharField(max_length=30)
+    due_date = models.CharField(max_length=20)
+    status = models.CharField(max_length = 30) 
 
     def publish(self):
         self.published_date = timezone.now()
